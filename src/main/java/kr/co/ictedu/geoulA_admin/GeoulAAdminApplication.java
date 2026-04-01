7a86bc16bb31514ff6013bf3a8d57583c4e54a73
jsw0227 <zhalrtjddn01@gmail.com>
2026-04-01 18:29:37 +0900

    JPA 기본 설정

--- src/main/java/kr/co/ictedu/geoulA_admin/GeoulAAdminApplication.java
+++ src/main/java/kr/co/ictedu/geoulA_admin/GeoulAAdminApplication.java
@@ -2,12 +2,29 @@ package kr.co.ictedu.geoulA_admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GeoulAAdminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeoulAAdminApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer crossConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				System.out.println("Test==================");
+				registry.addMapping("/**")
+				.allowedOrigins("http://192.168.0.54:3001/" , "http://localhost:3001/")
+				.allowedHeaders("*")
+				.allowedMethods("*").maxAge(3600);
+			}
+		};
 	}
 
 }
