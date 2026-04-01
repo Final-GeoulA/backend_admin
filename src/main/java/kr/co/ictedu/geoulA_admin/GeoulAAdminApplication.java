56ff7faad3892f32c9e46cb304676e08f25e4cfa
jsw0227 <zhalrtjddn01@gmail.com>
2026-04-01 18:11:41 +0900

    backend_admin 초기설정

---
+++ src/main/java/kr/co/ictedu/geoulA_admin/GeoulAAdminApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ictedu.geoulA_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoulAAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoulAAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/ictedu/geoulA_admin/GeoulAAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ictedu.geoulA_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoulAAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
